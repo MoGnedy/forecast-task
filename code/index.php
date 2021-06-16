e0f290d9349ceab8bb9686497c00b83a660cfee8
mognedy <mognedy@gmail.com>
2021-06-16 21:52:39 +0200

    Adapt cities to take the necessary attributes and to be easy to use.

--- code/index.php
+++ code/index.php
@@ -1,6 +1,7 @@
 <?php
 require "./vendor/autoload.php";
 
+use App\Adapters\CityForcastAdapter;
 use App\Clients\{MusementApiClient, WeatherApiClient};
 
 $musementApiClient = new MusementApiClient();
@@ -8,4 +9,12 @@ $cities = $musementApiClient->getCities();
 
 $weatherApiClient = new WeatherApiClient('eb0d3f37cff6415d860141752211206');
 $citiesForcast = $weatherApiClient->getCitiesForcast($cities, 2);
+
+
+$adaptedCities = [];
+
+foreach ($citiesForcast as $city) {
+    $adapter = new CityForcastAdapter($city);
+    $adaptedCities[] = $adapter->adapte();
+}
 
