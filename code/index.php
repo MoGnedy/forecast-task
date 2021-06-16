ab505c9dc0fabd4da43692000ef9187ef40ad2b5
mognedy <mognedy@gmail.com>
2021-06-16 21:53:42 +0200

    Print Cities with their weather on the main page.

--- code/index.php
+++ code/index.php
@@ -8,7 +8,7 @@ $musementApiClient = new MusementApiClient();
 $cities = $musementApiClient->getCities();
 
 $weatherApiClient = new WeatherApiClient('eb0d3f37cff6415d860141752211206');
-$citiesForcast = $weatherApiClient->getCitiesForcast($cities, 2);
+$citiesForcast = $weatherApiClient->getCititsForcast($cities, 2);
 
 
 $adaptedCities = [];
@@ -18,3 +18,8 @@ foreach ($citiesForcast as $city) {
     $adaptedCities[] = $adapter->adapte();
 }
 
+
+foreach ($adaptedCities as $cityForcast) {
+    echo "Processed city " . ($cityForcast['name']) . " - " . ($cityForcast['forecast_days'][0]) . " | " . ($cityForcast['forecast_days'][1]) . ".\r\n";
+    echo '<br>';
+}
