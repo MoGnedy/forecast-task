f3de7960879b040ce38f225f60230d3da253b184
mognedy <mognedy@gmail.com>
2021-06-16 21:50:19 +0200

    Retrieve Cities weather for today and tomorrow.

--- code/index.php
+++ code/index.php
@@ -1,9 +1,11 @@
 <?php
 require "./vendor/autoload.php";
 
-use App\Clients\MusementApiClient;
+use App\Clients\{MusementApiClient, WeatherApiClient};
 
 $musementApiClient = new MusementApiClient();
 $cities = $musementApiClient->getCities();
 
+$weatherApiClient = new WeatherApiClient('eb0d3f37cff6415d860141752211206');
+$citiesForcast = $weatherApiClient->getCitiesForcast($cities, 2);
 
